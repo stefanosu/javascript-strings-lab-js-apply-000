08037e515ec4b68c7e4977b5847476c09500df3d
Stefanos Ugbit <stefanosu422@gmail.com>
2019-03-13 19:25:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = `Hello, ${specialGuest}!`;
 
-var greetSpecialGuest = "Hello," + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
